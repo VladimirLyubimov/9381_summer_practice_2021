9cec2ee1790cf15c2faf1d914b2ca8d88aeab523
LyubimovVladimir <55154058+VladimirLyubimov@users.noreply.github.com>
2021-07-04 18:57:05 +0300

    Add files via upload

--- Astar/src/main/java/Main.java
+++ Astar/src/main/java/Main.java
@@ -7,11 +7,16 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hi");
         String[] edges = new String[] {"A B 2", "A E 2", "B D 1", "D C 1", "B A 2", "E A 2", "D B 1", "C D 1", "E F 1", "D F 4"};
-        String[] vertexes = new String[] {"A 0 0", "B 2 0" , "D 2 1", "C 1 1", "E 0 2", "F 0 3"};
+        String[] vertexes = new String[] {"A 0 0", "B 2 0" , "D 2 1", "C 1 1", "E 0 2", "F 0 3", "T 5 0"};
         MyGraph graph = new MyGraph(edges, vertexes);
         graph.printGraph();
         AWithStar algo = new AWithStar(graph);
-        ArrayList<Integer> path = algo.doAlgo("A", "F");
+        ArrayList<Integer> path = algo.doAlgo("A", "C");
+        if(path == null){
+            System.out.println("No path!");
+            return;
+        }
+        
         for(int num : path){
             System.out.print(num + " ");
         }
